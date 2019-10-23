f5e61ed7517a0188a7a3b1804ed967272c95a756
issam <issamelguerch@gmail.com>
2019-10-24 01:31:34 +0200

    fix refresh

--- app/src/main/java/com/app/hb7launcher/MainFragment.java
+++ app/src/main/java/com/app/hb7launcher/MainFragment.java
@@ -1,9 +1,10 @@
 package com.app.hb7launcher;
 
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v17.leanback.app.VerticalGridFragment;
@@ -17,6 +18,7 @@ import android.support.v17.leanback.widget.Row;
 import android.support.v17.leanback.widget.RowPresenter;
 import android.support.v17.leanback.widget.VerticalGridPresenter;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.app.hb7launcher.cards.presenters.CardPresenterSelector;
 import com.app.hb7launcher.model.AppModel;
@@ -32,12 +34,26 @@ public class MainFragment extends VerticalGridFragment {
     private static final int ZOOM_FACTOR = FocusHighlight.ZOOM_FACTOR_MEDIUM;
     private Context mContext=getContext();
     private static final String TAG = "MainFragmentLauncherHB7";
-    private Drawable mDefaultCardImage;
+    BroadcastReceiver mServiceReceiver;
     private ArrayObjectAdapter mAdapter;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupRowAdapter();
         setupEventListeners();
+        mServiceReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // when package removed
+                if (intent.getAction().equals("android.intent.action.PACKAGE_ADDED")) {
+                    addApp(context, intent);
+                }
+                // when package installed
+                else if (intent.getAction().equals("android.intent.action.PACKAGE_REMOVED")) {
+                    removeApp(context, intent);
+                }
+            }
+        };
+
     }
     private void setupRowAdapter() {
         VerticalGridPresenter gridPresenter = new VerticalGridPresenter(ZOOM_FACTOR);
@@ -51,6 +67,17 @@ public class MainFragment extends VerticalGridFragment {
         createRows();
         startEntranceTransition();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.intent.action.PACKAGE_REMOVED");
+        filter.addAction("android.intent.action.PACKAGE_ADDED");
+        filter.addDataScheme("package");
+        getActivity().registerReceiver(mServiceReceiver, filter);
+    }
+
     private void createSettings(){
         FunctionAppManage functionAppManage = new FunctionAppManage(getContext());
 
@@ -82,7 +109,13 @@ public class MainFragment extends VerticalGridFragment {
                 }else{
                      launchIntent = getActivity().getPackageManager().getLaunchIntentForPackage(
                             appBean.getPackageName());
-                    getActivity().startActivity(launchIntent);
+                    try {
+                        getActivity().startActivity(launchIntent);
+                    }catch (Exception e){
+                        Log.e("my launcher",e.getMessage());
+                        Toast.makeText(getContext(), "probleme dans l'Application",
+                                Toast.LENGTH_LONG).show();
+                    }
 
                 }
              }
@@ -105,8 +138,6 @@ public class MainFragment extends VerticalGridFragment {
                         Intent intent = getContext().getPackageManager().getLaunchIntentForPackage("com.android.tv.settings");
                         startActivity(intent);
                      }
-
-
                  }catch(Exception e){
                      Log.e("my launcher",e.getMessage());
                      startActivity(new Intent(Settings.ACTION_SETTINGS));
@@ -124,4 +155,42 @@ public class MainFragment extends VerticalGridFragment {
         }
     }
 
+    public void addApp(Context context, Intent intent) {
+        AppDataManage appDataManage = new AppDataManage(context);
+        if (!appDataManage.getArraylistPackage().contains(intent.getData().getSchemeSpecificPart())) {
+            appDataManage.getArraylistPackage().add(intent.getData().getSchemeSpecificPart());
+            appDataManage.saveListPackage();
+            mAdapter.clear();
+            prepareEntranceTransition();
+            createRows();
+            Log.e("BroadcastReceiver ", "InstallationReceiver onReceive called "
+                    + " PACKAGE_ADDED ");
+            Toast.makeText(context, "Application intallée: " + intent.getData().getSchemeSpecificPart(),
+                    Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    public void removeApp(Context context, Intent intent) {
+        AppDataManage appDataManage = new AppDataManage(context);
+        boolean isRemoved = appDataManage.getArraylistPackage().remove(intent.getData().getSchemeSpecificPart());
+        if (isRemoved) {
+            appDataManage.saveListPackage();
+            for (int i = 0; i < mAdapter.size(); i++) {
+                AppModel appModel;
+                if (mAdapter.get(i) instanceof AppModel) {
+                    appModel = (AppModel) mAdapter.get(i);
+                    if (appModel.getPackageName().equals(intent.getData().getSchemeSpecificPart())) {
+                        mAdapter.remove(appModel);
+                        break;
+                    }
+                }
+
+            }
+            Log.e(" BroadcastReceiver ", " InstallationReceiver onReceive called " + "PACKAGE_REMOVED :" + intent.getData().getSchemeSpecificPart());
+            Toast.makeText(context, "Application Supprimé: " + intent.getData().getSchemeSpecificPart(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
 }
